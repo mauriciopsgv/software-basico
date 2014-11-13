04d2326c77e934c57785d8cc912864dcce972835
mauriciopsgv <mauriciopsgv@gmail.com>
2014-11-13 16:34:34 -0200

    Same thing but better implemented

--- Mini-compiler/compiler.c
+++ Mini-compiler/compiler.c
@@ -10,17 +10,17 @@ typedef union num
 	unsigned char b[4];
 } N_union;
 
-
-void set_ret_code (Code codigo){
+void initialize_code (Code codigo){
 
 	codigo[0] = 0x55;							// push %ebp
 	codigo[1] = 0x89; 	codigo[2] = 0xe5;		// mov %esp, %ebp
-	codigo[3] = 0xb8;							// mov ...
-	
+}
+
+void finalize_code (Code codigo){
+
 	codigo[8] =0x89;	codigo[9] = 0xe5;		// mov %ebp, %esp
 	codigo[10] = 0x5d;							// pop %ebp
 	codigo[11] = 0xc3;							// ret
-
 }
 
 void read_ret (FILE* arq_fonte, Code codigo){
@@ -29,18 +29,23 @@ void read_ret (FILE* arq_fonte, Code codigo){
 	N_union num;
 	fscanf( arq_fonte, "et %c%d", &c, &num.i );
 
-	set_ret_code(codigo);
-
 	switch(c)
 	{
 		case '$':
 		{
+			codigo[3] = 0xb8;
 			codigo[4] = num.b[0];
 			codigo[5] = num.b[1];
 			codigo[6] = num.b[2];
 			codigo[7] = num.b[3];
 		}
+		case 'p':
+		{
+
+		}
 	}
+
+	finalize_code(codigo);
 
 }
 
@@ -48,6 +53,8 @@ funcp geracod(FILE* arq_fonte){
 
 	int c;
 	Code codigo = (Code) malloc(NUMERO_BEM_GRANDE);
+
+	initialize_code(codigo);
 
 	while ((c = fgetc(arq_fonte)) != EOF){
 
